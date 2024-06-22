3af11cb9663507b7f81626e997406889ed6f4140
DeadInsane5 <aditya.ajaykumar.2006@gmail.com>
2024-06-22 20:40:44 +0530

    added themes

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -19,7 +19,7 @@ return {
       cmp.setup({
         window = {
           completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
+          -- documentation = cmp.config.window.bordered(),
         },
       })
 
