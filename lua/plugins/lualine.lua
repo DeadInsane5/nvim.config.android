4459e5618142a194effe77f6345821cebc57523b
DeadInsane5 <aditya.ajaykumar.2006@gmail.com>
2024-07-02 22:39:23 +0530

    added lualine ascii customization

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,38 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  config = function()
+    local lualine = require("lualine")
+    lualine.setup {
+
+      sections = {
+        lualine_a = {
+          {
+            "mode",
+            icons_enabled = true,
+            separator = {
+              left = "",
+              right = ""
+              -- right = ""
+            },
+            fmt = function()
+              local mode_map = {
+                n = "(ᴗ_ ᴗ。) i sleep",
+                nt = "(ᴗ_ ᴗ。) i sleep",
+                i = "(•̀ - •́ ) real shitt",
+                R = "( •̯́ ₃ •̯̀)",
+                v = "(⊙ _ ⊙ ) DAMN",
+                V = "(⊙ _ ⊙ ) DAMN",
+                no = "Σ(°△°ꪱꪱꪱ) LETSS GOO",
+                ["\22"] = "(⊙ _ ⊙ ) DAMN",
+                t = "(⌐■_■) hecker",
+                ['!'] = "Σ(°△°ꪱꪱꪱ) LETSS GOO",
+                c = "Σ(°△°ꪱꪱꪱ) LETSS GOO",
+                s = "SUB"
+              }
+              return mode_map[vim.api.nvim_get_mode().mode] or vim.api.nvim_get_mode().mode
+            end },
+        },
+      }
+    }
+  end
+}
